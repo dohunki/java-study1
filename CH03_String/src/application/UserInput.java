dd675add5ea2d7672cf2306fb27d7833e012ee66
wi_deep <92368497+dohunki@users.noreply.github.com>
2021-11-18 12:41:52 +0900

    폴더명 변경

--- CH03_String/src/application/UserInput.java
+++ CH03_String/src/application/UserInput.java
@@ -8,15 +8,16 @@ public class UserInput {
 		// 입력을 받는 클래스
 		Scanner scanner= new Scanner(System.in);  //스캐너 객체를 선언
 		
-		System.out.println("온도를 입력해 주세요 : ");
+		System.out.print("온도를 입력해 주세요 : ");
 		int x= scanner.nextInt(); // 스캐너로 정수 입력을 받아 x에 입력, 
 		// 실행시 입력을 대기하고 엔터키가 입력되면 종료
 		System.out.println(x);
 		
+		System.out.print("화씨로 변환할 온도를 입력해 주세요 : ");
 		double c= scanner.nextDouble();
 		double f= (c* 9/5)+ 32;
 		
-		System.out.println("섭씨" + c + "는 화씨" + f +"이다.");
+		System.out.printf("섭씨 %.1f는 화씨 %.1f 이다.", c, f);
 		
 
 	}
