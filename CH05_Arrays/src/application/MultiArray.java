8256c706c8e0dfcc29a3ed890db3f2681a781631
wi_deep <92368497+dohunki@users.noreply.github.com>
2021-11-19 16:44:38 +0900

    11월 18일 오후

--- CH05_Arrays/src/application/MultiArray.java
+++ CH05_Arrays/src/application/MultiArray.java
@@ -13,7 +13,6 @@ public class MultiArray {
 		for (int i= 0; i< texts.length; i++) {
 			String[] inArray= texts[i];
 			System.out.printf("%d번째 배열 : %s\n", i, inArray);
-			System.out.println(texts[i]);
 			for(int j= 0; j< inArray.length; j++) {
 				System.out.printf("%s\t", inArray[j]);
 			}
